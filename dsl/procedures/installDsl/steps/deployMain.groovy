b3fbffbe8c48e2739d4f9c1262dd7a8516235a0a
Laurent Rochette <lrochette@eletric-cloud.com>
2018-08-02 19:25:54 -0700

    evalDsl the top main.groovy file

--- dsl/procedures/installDsl/steps/deployMain.groovy
+++ dsl/procedures/installDsl/steps/deployMain.groovy
@@ -1,3 +1,20 @@
-//
+/*
+  deployMain.groovy - deploy the top level main.groovy if it exists
 
-ectool evalDsl --dslFile main.groovy
+  Copyright 2018 Electric-Cloud Inc.
+
+  CHANGELOG
+  ----------------------------------------------------------------------------
+  2018-08-03  lrochette Initial Version
+
+*/
+
+import java.io.File
+import com.electriccloud.client.groovy.ElectricFlow
+
+ElectricFlow ef = new ElectricFlow()
+
+File topDSLFile = new File(".", 'main.groovy')
+if (topDSLFile.exists()) {
+  ef.evalDsl(dslFile: topDSLFile)
+}
