9e10edc2d966a0464ea1788113fcf2aefec894ca
Laurent Rochette <lrochette@eletric-cloud.com>
2018-09-27 12:36:03 -0500

    0.8.0.245: Add mechanism for broadcast

--- dsl/procedures/installDsl/procedure.groovy
+++ dsl/procedures/installDsl/procedure.groovy
@@ -4,7 +4,12 @@ def procName = 'installDsl'
 procedure procName, {
 
 	step 'retrieveArtifact',
-		subproject: '/plugins/EC-Artifact/project',
+   command: new File(pluginDir, "dsl/procedures/$procName/steps/retrieveArtifact.pl").text,
+   errorHandling: 'abortProcedure',
+   resourceName: '$[pool]',
+   shell: 'ec-perl'
+/*
+    subproject: '/plugins/EC-Artifact/project',
 		subprocedure: 'retrieve',
 		actualParameter:[
 			artifactName: '$[artName]',
@@ -12,14 +17,17 @@ procedure procName, {
 			artifactVersionLocationProperty: '/myJob/retrievedArtifactVersions/$[assignedResourceName]',
 			retrieveToDirectory: "."
 	]
+*/
 
 	//evalDsl the main.groovy if it exists
 	step 'deployMain',
     command: new File(pluginDir, "dsl/procedures/$procName/steps/deployMain.groovy").text,
-    shell: 'ec-groovy'
+    shell: 'ec-groovy',
+    resourceName: '$[/myJob/assignedResourceName]'
 
 	// loop on each project
 	step 'deployProjects',
 		command: new File(pluginDir, "dsl/procedures/$procName/steps/deployProjects.groovy").text,
-		shell: 'ec-groovy'
+    resourceName: '$[/myJob/assignedResourceName]',
+    shell: 'ec-groovy'
 }
---
+++ dsl/procedures/installDsl/steps/retrieveArtifact.pl
@@ -0,0 +1,63 @@
+##############################################################################
+# retrieveArtifacts.pl - Need a explicit step instead of a plugin call
+#       due to the act you can not broadcast a plugin call nor choose the
+#       resource
+#
+# Copyright 2018 Electric-Cloud Inc.
+#
+# CHANGELOG
+# ----------------------------------------------------------------------------
+# 2018-09-27  lrochette Initial Version
+#
+##############################################################################
+$[/plugins/EC-Admin/project/scripts/perlHeaderJSON]
+
+#
+# Parameters
+#
+my $pool='$[pool]';
+my $broadcast='$[allNodes]';
+#
+# retrieve the artifact version on one resource
+sub retrieve {
+  my $res=shift;
+  $ec->createJobStep({
+    jobStepName  => "$res",
+    resourceName => "$res",
+    subproject   => '/plugins/EC-Artifact/project',
+    subprocedure => 'retrieve',
+    actualParameter => [
+			{actualParameterName => 'artifactName', value => '$[artName]'},
+			{actualParameterName => 'versionRange', value => '$[artVersion]'},
+			{actualParameterName => 'artifactVersionLocationProperty', value => "/myJob/retrievedArtifactVersions/$res"},
+			{actualParameterName => 'retrieveToDirectory', value => "."}
+ 	]
+  });
+}
+
+my $assignedResourceName='$[/myJobStep/assignedResourceName]';
+$ec->setProperty("/myJob/assignedResourceName", $assignedResourceName);
+#
+# If no broadcast,simply pick one resource out of the pool
+if ($broadcast eq "false") {
+    $ec->setProperty("summary", "No broadcast");
+    retrieve($assignedResourceName);
+    exit(0);
+}
+
+my ($success, $result, $errMsg, $errCode) =
+  InvokeCommander("IgnoreError", 'getResourcePool', $pool);
+
+# A simple resource
+if (! $success) {
+  $ec->setProperty("summary", "Broadcast on simple resource $pool");
+  retrieve($pool);
+  exit(0);
+}
+
+$ec->setProperty("summary", "Broadcast on pool $pool");
+foreach my $res ($result->findnodes('//resourceName')) {
+  retrieve($res)
+}
+
+$[/plugins/EC-Admin/project/scripts/perlLibJSON]
