746c2be276337e925589d34bf0a8d4725d12a728
Urvashi Singh <urvashisingh@yahoo.com>
2019-03-05 12:44:33 -0800

    Fix for NMB-27865: DSL evaluation issue using generateDsl format <argOrProp> = <value>
    Use the same delegate DslDelegate and the same groovy class loader for parsing and executing the nested DSL scripts that are used for evaluating the DSL passed to evalDsl.

--- sample/repo/projects/FOO/procedures/BAR/procedure.groovy
+++ sample/repo/projects/FOO/procedures/BAR/procedure.groovy
@@ -2,10 +2,29 @@ import java.io.File
 
 def procName = 'BAR'
 procedure procName, {
+  step 'echo', {
+    command = new File(projectDir, "./procedures/$procName/steps/echo.pl").text
+    shell = 'ec-perl'
 
-	//evalDsl the main.groovy if it exists
-	step 'echo',
-    command: new File(projectDir, "./procedures/$procName/steps/echo.pl").text,
-    shell: 'ec-perl'
+    // Added for testing NMB-27865.
+    // Check that both these property sheets
+    // are created with the nested properties
+	property 'Framework', {
+ 
+      // Custom properties
+      property 'frmSvcImageTag', value:'1.0.80'
+    }
+ 
+    property 'QC', {
+      description = 'Quality Center'
+ 
+      // Custom properties
+      qc_base_path = '/qcbin'
+      qc_rest_authentication_point = '/authentication-point/authenticate'
+      qc_rest_base_path = '/qcbin/rest'
+      qc_rest_hostname = 'qc'
+      qc_rest_protocol = 'http'
+    }
+  }
 
 }
