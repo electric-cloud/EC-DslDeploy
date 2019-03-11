74025e0221239469243c314e68373209802662ab
Laurent Rochette <lrochette@eletric-cloud.com>
2019-03-11 10:23:13 -0500

    Adding tests for NMB-27865

--- specs/src/test/groovy/com/electriccloud/plugin/spec/NMB27865.groovy
+++ specs/src/test/groovy/com/electriccloud/plugin/spec/NMB27865.groovy
@@ -5,7 +5,7 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
-import groovy.util.BuildLogger
+import org.apache.tools.ant.BuildLogger
 
 class NMB27865 extends PluginTestHelper {
   static String pName='EC-DslDeploy'
