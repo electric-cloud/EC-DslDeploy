a44a68d979a406eab7171801a69d493ccac943a4
Laurent Rochette <lrochette@eletric-cloud.com>
2019-03-11 10:05:35 -0500

    Adding tests for NMB-27865

--- specs/src/test/groovy/com/electriccloud/plugin/spec/NMB27865.groovy
+++ specs/src/test/groovy/com/electriccloud/plugin/spec/NMB27865.groovy
@@ -1,13 +1,13 @@
 package com.electriccloud.plugin.spec
 import spock.lang.*
-import java.nio.file.Files
+import java.nio.file.*
 
 class NMB27865 extends PluginTestHelper {
   static String pName='EC-DslDeploy'
   static NMB="NMB27865"
 
   def doSetupSpec() {
-    Files.copy("resources/$NMB", "/tmp/$NMB", Files.REPLACE_EXISTING)
+    Files.copy("resources/$NMB", "/tmp/$NMB", REPLACE_EXISTING)
   }
 
   def doCleanupSpec() {
