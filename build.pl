b54321d106dbb2527053be5850ff2dd4170e181f
Valeriy Fezler <karakurty@gmail.com>
2022-08-05 16:03:39 +0200

    BEE-22251 Import with EC-DslDeploy is broken

--- build.pl
+++ build.pl
@@ -15,7 +15,7 @@ my $ec = new ElectricCommander->new();
 
 my $epb="../ecpluginbuilder";
 
-my $pluginVersion = "4.1.2";
+my $pluginVersion = "4.1.3";
 my $pluginKey = "EC-DslDeploy";
 
 # Fix version in plugin.xml
