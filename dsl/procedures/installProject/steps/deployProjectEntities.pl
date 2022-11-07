249f8287d345f5f2053869f08920136f7483ece8
Valeriy Fezler <karakurty@gmail.com>
2022-11-07 17:12:16 +0100

    BEE-27841 Instrument EC-DslDeploy code to help debug HSBC performance issue where importing a release from a nested dsl file structure

--- dsl/procedures/installProject/steps/deployProjectEntities.pl
+++ dsl/procedures/installProject/steps/deployProjectEntities.pl
@@ -21,6 +21,14 @@ if ("$timeout" eq "") {
 }
 print("Timeout is: '$timeout'\n");
 
+my ($debug) = ("$[additionalDslArguments]" =~ m/--debug\s+([0-1]+)/);
+print("User $debug is: '$debug'\n");
+
+if ("$debug" eq "") {
+    $debug = 0;
+}
+print("User $debug is: '$debug'\n");
+
 # Is this an incremental import?
 my $incremental = 0;
 $ec->abortOnError(0);
@@ -75,7 +83,7 @@ foreach my $objectType (@subProjectEntities ) {
         }
     }
 
-    my $shell   = 'ectool --timeout ' . $timeout . ' evalDsl --dslFile {0}.groovy --serverLibraryPath "$[/server/settings/pluginsDirectory]/$[/myProject/projectName]/dsl" $[additionalDslArguments]';
+    my $shell   = 'ectool --timeout ' . $timeout . ' --debug ' . $debug . ' evalDsl --dslFile {0}.groovy --serverLibraryPath "$[/server/settings/pluginsDirectory]/$[/myProject/projectName]/dsl" $[additionalDslArguments]';
 
     # without Perl variables usage / substitution
     my $command1 = <<'END_COMMAND';
