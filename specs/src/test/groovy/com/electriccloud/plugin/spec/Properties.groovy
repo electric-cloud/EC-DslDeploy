e1df64128db8a6a7e005c761e1077bc4d3b9cfd5
Valeriy Fezler <karakurty@gmail.com>
2023-04-28 09:57:09 +0200

    BEE-33566 EC-DslDeploy: installDslFromDirectory missed property with question mark symbol in name after import

--- specs/src/test/groovy/com/electriccloud/plugin/spec/Properties.groovy
+++ specs/src/test/groovy/com/electriccloud/plugin/spec/Properties.groovy
@@ -262,7 +262,6 @@ class Properties
 
     cleanup:
     deleteProjects([projectName: jira], false)
-    deleteProjects([mainProject: 'projectName ?<>%*(!@#$^&()|:\\ projectName'])
     new File(dslDir).deleteDir()
   }
 }
